4519865df48702b693dbcd5679fc4b521cf6a4ec
Kien Luu <kien.luu@base79.com>
2013-06-08 15:14:07 +0100

    no db changes

--- views.py
+++ views.py
@@ -1,13 +1,104 @@
 # Create your views here.
 import mimetypes
 from django.core.servers.basehttp import FileWrapper
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django import http
 from django.utils.encoding import smart_str
-from django.views.generic import View
+# from django.views.generic import View
 from .storage import BlobStoreStorage
 from google.appengine.api.images import NotImageError, get_serving_url
 import os
 
+from django.utils.decorators import classonlymethod
+from django.utils import six
+
+
+import logging
+from functools import update_wrapper
+
+logger = logging.getLogger('django.request')
+
+
+class View(object):
+    """
+    Intentionally simple parent class for all views. Only implements
+    dispatch-by-method and simple sanity checking.
+    """
+
+    http_method_names = ['get', 'post', 'put', 'delete', 'head', 'options', 'trace']
+
+    def __init__(self, **kwargs):
+        """
+        Constructor. Called in the URLconf; can contain helpful extra
+        keyword arguments, and other things.
+        """
+        # Go through keyword arguments, and either save their values to our
+        # instance, or raise an error.
+        for key, value in six.iteritems(kwargs):
+            setattr(self, key, value)
+
+    @classonlymethod
+    def as_view(cls, **initkwargs):
+        """
+        Main entry point for a request-response process.
+        """
+        # sanitize keyword arguments
+        for key in initkwargs:
+            if key in cls.http_method_names:
+                raise TypeError("You tried to pass in the %s method name as a "
+                                "keyword argument to %s(). Don't do that."
+                                % (key, cls.__name__))
+            if not hasattr(cls, key):
+                raise TypeError("%s() received an invalid keyword %r. as_view "
+                                "only accepts arguments that are already "
+                                "attributes of the class." % (cls.__name__, key))
+
+        def view(request, *args, **kwargs):
+            self = cls(**initkwargs)
+            if hasattr(self, 'get') and not hasattr(self, 'head'):
+                self.head = self.get
+            self.request = request
+            self.args = args
+            self.kwargs = kwargs
+            return self.dispatch(request, *args, **kwargs)
+
+        # take name and docstring from class
+        update_wrapper(view, cls, updated=())
+
+        # and possible attributes set by decorators
+        # like csrf_exempt from dispatch
+        update_wrapper(view, cls.dispatch, assigned=())
+        return view
+
+    def dispatch(self, request, *args, **kwargs):
+        # Try to dispatch to the right method; if a method doesn't exist,
+        # defer to the error handler. Also defer to the error handler if the
+        # request method isn't on the approved list.
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
+
+    def http_method_not_allowed(self, request, *args, **kwargs):
+        logger.warning('Method Not Allowed (%s): %s', request.method, request.path,
+            extra={
+                'status_code': 405,
+                'request': self.request
+            }
+        )
+        return http.HttpResponseNotAllowed(self._allowed_methods())
+
+    def options(self, request, *args, **kwargs):
+        """
+        Handles responding to requests for the OPTIONS HTTP verb.
+        """
+        response = http.HttpResponse()
+        response['Allow'] = ', '.join(self._allowed_methods())
+        response['Content-Length'] = '0'
+        return response
+
+    def _allowed_methods(self):
+        return [m.upper() for m in self.http_method_names if hasattr(self, m)]
 
 class BlobStoreFileServeView(View):
 
@@ -19,13 +110,13 @@ class BlobStoreFileServeView(View):
         if storage.exists(key):
             mimetype, encoding = mimetypes.guess_type(kwargs['key']) or\
                                  ("application/x-octet-stream", None)
-            response = HttpResponse(
+            response = http.HttpResponse(
                 FileWrapper(storage.open(key)), mimetype=mimetype)
             if request.GET.get('download') in self._boolean_yes_states:
                 response['Content-Disposition'] = smart_str(
                         u'attachment; filename=%s' % os.path.basename(key))
         else:
-            response = HttpResponseNotFound()
+            response = http.HttpResponseNotFound()
         return response
 
 
@@ -44,10 +135,10 @@ class BlobStoreImageServeView(View):
         storage = BlobStoreStorage()
         try:
             # Default serving size is 512
-            return HttpResponseRedirect(
+            return http.HttpResponseRedirect(
                 get_serving_url(storage._get_blobinfo(key), size=size))
         except NotImageError:
-            return HttpResponseNotFound()
+            return http.HttpResponseNotFound()
 
         """
         Size Available:
